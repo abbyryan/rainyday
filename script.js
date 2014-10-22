d792f5f5d960b5a046f5efffd6cf8db8cec43397
hilarylbeck <hilarybeck@me.com>
2014-10-22 14:32:32 -0400

    new jquery: toggling class for lightbox

--- script.js
+++ script.js
@@ -3,24 +3,10 @@ $(document).ready(function() {
 		/* lightbox appears */
 		e.preventDefault();
 		var image_href = $(this).attr("href");
-		if ($('.lightbox').length > 0) { /* lightbox active */
-			$('#content').html('<img src="' + image_href + '" />');
-			$('.lightbox').show();
-		}
-		else { /* lightbox not yet active -- no photo has been clicked */
-			var theLightbox =
-			'<div class="lightbox">' +
-                        '<p>click to close</p>' +
-                '<div id="content">' + 
-                    '<img src="' + image_href +'" />' +
-                '</div>' +
-            '</div>';
-            $('body').append(theLightbox);
-		}
-	});
-		/* hide lightbox */
-
+		$('.lightbox img').attr("src", image_href);
+		$('body').toggleClass('active');
+	});	
 	$('.lightbox').click(function() {
-		$(this).hide();
+		$('body').removeClass('active');
 	});
 });
