efc9417a2efc57be5dcfa45c144a4568aa674c30
hilarylbeck <hilarybeck@me.com>
2014-10-22 13:58:43 -0400

    jquery: activated lightbox

--- script.js
+++ script.js
@@ -1,7 +1,26 @@
 $(document).ready(function() {
-	$('.modal').click(function(e)) {
+	$('.modal').click(function(e) {
 		/* lightbox appears */
 		e.preventDefault();
-		
-	}
+		var image_href = $(this).attr("href");
+		if ($('.lightbox').length > 0) { /* lightbox active */
+			$('#content').html('<img src="' + image_href + '" />');
+			$('.lightbox').show();
+		}
+		else { /* lightbox not yet active -- no photo has been clicked */
+			var theLightbox =
+			'<div class="lightbox">' +
+                        '<p>click to close</p>' +
+                '<div id="content">' + 
+                    '<img src="' + image_href +'" />' +
+                '</div>' +
+            '</div>';
+            $('body').append(theLightbox);
+		}
+	});
+		/* hide lightbox */
+
+	$('.lightbox').click(function() {
+		$(this).hide();
+	});
 });
