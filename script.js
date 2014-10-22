b57ecc6a5aa8bc02643764fcb9811dcc926f0998
hilarylbeck <hilarybeck@me.com>
2014-10-22 17:29:42 -0400

    commented js

--- script.js
+++ script.js
@@ -1,32 +1,31 @@
 $(document).ready(function() {
-	$('.modal').click(function(e) {
-		/* lightbox appears */
-		e.preventDefault();
-		var image_href = $(this).attr("href");
-		$('.lightbox img').attr("src", image_href);
-		$('body').addClass('active');
-		$('#container ul li').removeClass('current');
-		$(this).parents('li').addClass('current');
+	$('.modal').click(function(e) { /* each li > a has class "modal". on clicking a modal link... */		
+		e.preventDefault(); /* prevent default behavior of clicking a link (opening the link in same page) */
+		var image_href = $(this).attr("href"); /* gets reference link from "modal" a */
+		$('.lightbox img').attr("src", image_href); /* attributes the clicked link to the img in "lightbox" div */
+		$('body').addClass('active'); /* "active" class sets "lightbox" display to block (originally none) */
+		$('#container ul li').removeClass('current'); /* if any previously clicked a's li has "current" class, remove it */
+		$(this).parents('li').addClass('current'); /* adds "current" class to clicked link */
 	});	
-	$('.lightbox').click(function() {
-		$('body').removeClass('active');
-		$('li').removeClass('current');
+	$('.lightbox').click(function() { /* click anywhere within active lightbox */
+		$('body').removeClass('active'); /* remove "active" class - display is back to none */
+		$('li').removeClass('current'); /* removes "current" class from originally clicked li */
 	});
-	$('body').on('keyup', function(e){
-		e.preventDefault();
-		if (e.keyCode == 37) {
-			if ($('body').hasClass('active')) {
-				$('#container ul li.current').prev().find('a').trigger('click');
+	$('body').on('keyup', function(e){ /* keyboard navigation */
+		e.preventDefault(); /* prevent default behavior of key you press */
+		if (e.keyCode == 37) { /* 37 is keycode for left arrow */
+			if ($('body').hasClass('active')) { /* makes sure lightbox is active and displayed */
+				$('#container ul li.current').prev().find('a').trigger('click'); /* whichever li is currently clicked on, triggers the "click" behavior above to previous li */
 			};
 		}
-		else if (e.keyCode == 39) {
-			if ($('body').hasClass('active')) {
-				$('#container ul li.current').next().find('a').trigger('click');
+		else if (e.keyCode == 39) { /* 39 is keycode for left arrow */
+			if ($('body').hasClass('active')) { /* makes sure lightbox is active and displayed */
+				$('#container ul li.current').next().find('a').trigger('click'); /* whichever li is currently clicked on, triggers the "click" behavior above to next li */
 			};
 		}
-		else if (e.keyCode == 27) {
-			if ($('body').hasClass('active')) {
-				$('.lightbox').trigger('click');
+		else if (e.keyCode == 27) { /* 27 is keycode for esc */
+			if ($('body').hasClass('active')) { /* makes sure lightbox is active and displayed */
+				$('.lightbox').trigger('click'); /*triggers lightbox click behavior: removes classes */
 			};
 		}		
 	});
