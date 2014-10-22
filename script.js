b5931d90fa46deb118fbe2605cf600fd5081d717
hilarylbeck <hilarybeck@me.com>
2014-10-22 16:43:01 -0400

    added jquery keyboard navigation for lightbox

--- script.js
+++ script.js
@@ -4,9 +4,25 @@ $(document).ready(function() {
 		e.preventDefault();
 		var image_href = $(this).attr("href");
 		$('.lightbox img').attr("src", image_href);
-		$('body').toggleClass('active');
+		$('body').addClass('active');
+		$('#container ul li').removeClass('current');
+		$(this).parents('li').addClass('current');
 	});	
 	$('.lightbox').click(function() {
 		$('body').removeClass('active');
+		$('li').removeClass('current');
+	});
+	$('body').on('keyup', function(e){
+		e.preventDefault();
+		if (e.keyCode == 37) {
+			if ($('body').hasClass('active')) {
+				$('#container ul li.current').prev().find('a').trigger('click');
+			};
+		}
+		else if (e.keyCode == 39) {
+			if ($('body').hasClass('active')) {
+				$('#container ul li.current').next().find('a').trigger('click');
+			};
+		}
 	});
 });
